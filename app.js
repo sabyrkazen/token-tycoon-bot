9eeda57a6cbc031f55fc8c0bc07549c3f47f511a
Sabyrka <sabyrkazen@gmail.com>
2026-02-23 04:33:44 +0500

    feat: enhance start command to include a greeting and inline button for web app access

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const webAppUrl = 'https://token-tycoon-clicker.web.app/'
 
 const bot = new Telegraf(token)
 
-bot.command('start', (ctx) => {})
+bot.command('start', (ctx) => {
+  ctx.reply(
+    'Hello! Press to start the app',
+    Markup.inlineKeyboard([
+      Markup.button.webApp('Open App', `${webAppUrl}?ref=${ctx.payload}`),
+    ]),
+  )
+})
 
 bot.launch()
